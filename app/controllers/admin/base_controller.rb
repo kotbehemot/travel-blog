365052ca804ee79895784c116d567b920dc25350
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-04-06 20:57:01 +0200

    add password setting for production

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,3 @@
 class Admin::BaseController < ApplicationController
-  http_basic_authenticate_with :name => "user", :password => "secret"
+  http_basic_authenticate_with :name => ENV["ADMIN_USERNAME"] || "user", :password => ENV["ADMIN_PASSWORD"] || "secret"
 end
