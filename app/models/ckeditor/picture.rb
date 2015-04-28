472cdfa966628114d8740173fbe6f07138428414
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-04-28 22:09:15 +0200

    testing assets

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -1,10 +1,10 @@
 class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
-                    :path => "/ckeditor_assets/pictures/:id_partition/:style/:filename",
-                    :styles => { :content => '800>', :thumb => '118x100#' }
+                    :path => (Rails.env.production? ? "/ckeditor_assets/pictures/:id_partition/:style/:filename" : ":rails_root/public/system/ckeditor_assets/pictures/:id_partition/:style/:filename"),
+                    :styles => { :content => '1600x1600>', :thumb => '118x100#', :original => '1600x1600>' }
 
   validates_attachment_presence :data
-  validates_attachment_size :data, :less_than => 2.megabytes
+  validates_attachment_size :data, :less_than => 5.megabytes
   validates_attachment_content_type :data, :content_type => /\Aimage/
 
   def url_content
