9fe5ff4c4a8f921084ad19228baf3cb674a82330
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-05-18 14:29:36 +0200

    fix rewrite

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.insert_before(::Rack::Runtime, ::Rack::Rewrite) do
   r301 %r{.*}, "#{ENV["CANONICAL_HOST"]}$&", :if => Proc.new {|rack_env|
-    rack_env['SERVER_NAME'] != ENV["CANONICAL_HOST"]
+    Regexp.new(rack_env['SERVER_NAME']).match(ENV["CANONICAL_HOST"]).nil?
   }
-end if false && Rails.env == 'production' && ENV["CANONICAL_HOST"]
+end if Rails.env == 'production' && ENV["CANONICAL_HOST"]
