8f3bde0dafc4df1f1506dd50edbdd065c69ad089
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-05-18 14:23:38 +0200

    tmp fix

--- config/initializers/rack_rewrite.rb
+++ config/initializers/rack_rewrite.rb
@@ -2,4 +2,4 @@ Rails.application.config.middleware.insert_before(::Rack::Runtime, ::Rack::Rewri
   r301 %r{.*}, "#{ENV["CANONICAL_HOST"]}$&", :if => Proc.new {|rack_env|
     rack_env['SERVER_NAME'] != ENV["CANONICAL_HOST"]
   }
-end if Rails.env == 'production' && ENV["CANONICAL_HOST"]
+end if false && Rails.env == 'production' && ENV["CANONICAL_HOST"]
