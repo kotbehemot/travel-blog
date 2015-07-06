0b6a28139aae059c67396efe2274ea4669a85ccb
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-07-06 17:11:28 +0300

    fix wrap problem

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < ApplicationController
 
   def show
     @homepage_photos = HomepagePhoto.order('created_at DESC').map {|photo| photo.image.url}
-    @locations = Location.safe.order('emailed_at ASC').map {|l| {:lat => l.lat, :lon => l.lon, :date => l.emailed_at.to_s(:short)} }
+    @locations = Location.safe.order('emailed_at DESC').map {|l| {:lat => l.lat, :lon => l.lon, :date => l.emailed_at.to_s(:short)} }
     @latest_post = Post.where(:published => true).order('created_at DESC').limit(1).first
   end
 
