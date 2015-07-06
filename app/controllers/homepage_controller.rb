ee72072c64dbd0bd6daf1334faa7f94ba9851bca
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2015-07-06 14:25:18 +0200

    display recently added post on homepage, not based on publication date

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,9 @@
 class HomepageController < ApplicationController
 
   def show
-    #@posts = Post.where(:published => true).order('counter_cache DESC').limit(5)
     @homepage_photos = HomepagePhoto.order('created_at DESC').map {|photo| photo.image.url}
     @locations = Location.safe.order('emailed_at ASC').map {|l| {:lat => l.lat, :lon => l.lon, :date => l.emailed_at.to_s(:short)} }
-    @latest_post = Post.published.first
+    @latest_post = Post.where(:published => true).order('created_at DESC').limit(1).first
   end
 
 end
