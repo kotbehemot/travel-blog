53f8f167a5d274895d348e873c74f02ff4481134
Piotr Barczuk <piotr.wiktor.barczuk@gmail.com>
2016-05-17 21:06:13 +0200

    parametrize google maps on dev/prd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  #load google maps api
+  config.x.load_google_maps = true
+
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
