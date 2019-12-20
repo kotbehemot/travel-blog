c2425f2abad38d98bc7905297bfd9ab379b7c99e
Piotr Barczuk <piotr.barczuk@upnext.io>
2019-12-20 12:01:52 +0100

    resolve problem with assets without extension breaking sprockets

--- config/application.rb
+++ config/application.rb
@@ -39,19 +39,5 @@ module TravelBlog
     config.i18n.available_locales = [:pl, :en]
     config.i18n.default_locale = :pl
     config.i18n.fallbacks = true
-
-    initializer 'setup_asset_pipeline', :group => :all  do |app|
-      # We don't want the default of everything that isn't js or css, because it pulls too many things in
-      app.config.assets.precompile.shift
-
-      # Explicitly register the extensions we are interested in compiling
-      app.config.assets.precompile.push(Proc.new do |path|
-        File.extname(path).in? [
-          '.html', '.erb', '.haml',                 # Templates
-          '.png',  '.gif', '.jpg', '.jpeg',         # Images
-          '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
-        ]
-      end)
-    end
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( admin.js admin.css ckeditor/* kube.min.js kube.min.css )
+Rails.application.config.assets.precompile += %w( admin.js admin.css ckeditor/*.* kube.min.js kube.min.css )
