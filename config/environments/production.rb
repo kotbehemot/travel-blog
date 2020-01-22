f3b9b0bace3ebe5d519df0aa5f170a3cf69243b8
Piotr Barczuk <piotr.barczuk@upnext.io>
2020-01-22 15:56:50 +0100

    more s3 configuration fixes = revert last changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,6 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :s3_region => ENV['AWS_REGION'],
-    },
-    :s3_host_name => "#{ENV['AWS_REGION']}.amazonaws.com",
-    :url => ":s3_host_name"
+    }
   }
 end
